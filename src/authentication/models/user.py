796d76ece2b23e41daf57e0cc3873842de6ac43c
zeldris <cesarcarrieldev@gmail.com>
2024-07-18 10:27:53 -0400

    refactor: str do modelo de user

--- src/authentication/models/user.py
+++ src/authentication/models/user.py
@@ -49,7 +49,7 @@ class User(AbstractBaseUser, PermissionsMixin, LogMixin):
         verbose_name = 'usuário'
 
     def __str__(self):
-        return f'{self.name} - {self.email}'
+        return f'{self.name} - {self.email}' if self.name else self.email
 
     def clean(self):
         super().clean()
