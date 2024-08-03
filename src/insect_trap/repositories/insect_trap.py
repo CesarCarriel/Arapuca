aef29c6077f866a84e92814a8e89beb1e7bc7eee
zeldris <cesarcarrieldev@gmail.com>
2024-08-03 16:38:39 -0400

    feat: campo calculado high_infestation que indica se uma armadilha tem um alto nivel de infestação

--- src/insect_trap/repositories/insect_trap.py
+++ src/insect_trap/repositories/insect_trap.py
@@ -2,11 +2,13 @@ from django.db import models
 from django.db.models.functions import Concat
 from django.db.models.query import QuerySet
 
-from insect_trap.models import InsectTrap
+from insect_trap.models import InsectTrap, InsectTrapResult
 
 
 class InsectTrapRepository:
     def list_all(self) -> QuerySet[InsectTrap]:
+        latest_results = InsectTrapResult.objects.filter(insect_trap=models.OuterRef('pk')).order_by('-created_at')
+
         insect_traps = InsectTrap.objects.annotate(
             rural_property_code=Concat(
                 models.F('field__tract__rural_property__code'),
@@ -23,6 +25,14 @@ class InsectTrapRepository:
                 models.F('type__insect__name'),
                 models.Value(')'),
                 output_field=models.CharField()
+            ),
+            latest_result=models.Subquery(latest_results.values('insect_number')[:1]),
+            high_infestation=models.Case(
+                models.When(
+                    type__infestation_alert_level__lte=models.F('latest_result'),
+                    then=models.Value(True)
+                ),
+                default=models.Value(False)
             )
         ).all()
 
